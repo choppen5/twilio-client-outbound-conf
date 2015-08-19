cdf22f307eec7599b8592b364f109ecedfbc62ff
choppen5 <choppen5@yahoo.com>
2015-08-19 10:45:44 -0700

    moved pusher vars to env variables

--- server.rb
+++ server.rb
@@ -2,6 +2,9 @@ require 'rubygems'
 require 'sinatra'
 require 'twilio-ruby'
 require 'logger'
+require 'pusher'
+require 'em-http-request'
+
 
 logger = Logger.new(STDOUT)
 logger.level = Logger::DEBUG
@@ -13,7 +16,15 @@ default_client = "charles"
 caller_id   = ENV['twilio_caller_id']
 account_sid = ENV['twilio_account_sid']
 auth_token  = ENV['twilio_auth_token']
-appsid      = ENV['twilio_app_id']
+appsid      = ENV['twilio_app_id_outbound']
+
+pusher_key      = ENV['pusher_key']
+pusher_secret   = ENV['pusher_secret']
+pusher_appid    = ENV['pusher_appid']
+
+Pusher.url = "http://#{pusher_key}:#{pusher_secret}@api.pusherapp.com/apps/#{pusher_appid}"
+
+
 
 get '/' do
     client_name = params[:client]
@@ -26,7 +37,7 @@ get '/' do
     capability.allow_client_outgoing appsid
     capability.allow_client_incoming client_name
     token = capability.generate
-    erb :index, :locals => {:token => token, :client_name => client_name}
+    erb :index, :locals => {:token => token, :client_name => client_name, :pusher_key => pusher_key}
 end
  
 
@@ -49,13 +60,41 @@ post '/customer-join-conf' do
     #do routing to find an agent, or use default
     #agentname = functiontofindavailibleagent
     agentname = default_client
+    Pusher[default_client].trigger_async('outbound_call', {
+        message: "Customer Call! You are connected to #{params['To']}"
+    })
+
+    logger.debug("sent pusher message, generating Twiml")
 
     response = Twilio::TwiML::Response.new do |r|
-        r.Dial do |d|
+        r.Dial(:action => '/after-customer-call') do |d|
                 d.Conference agentname, :beep => "onExit"
             end
     end
-    puts response.text
+    logger.debug(response.text)
     response.text
 end
+
+post '/make-dial-request' do
+    dialto = params[:dialnumber] #passed from the client (or more likely a auto dialer manager server component)
+    client = Twilio::REST::Client.new account_sid, auth_token
+
+    client.account.calls.create({
+          :from => caller_id,
+          :to => dialto,
+          :url => "#{request.base_url}/customer-join-conf"
+        })
+
+    return ""
+end 
+
+post '/after-customer-call' do
+    logger.debug("after customer call #{params}")
+
+    Pusher[default_client].trigger('outbound_call', {
+        message: "Customer Call complete!"
+    })
+
+    return ""
+end 
 
